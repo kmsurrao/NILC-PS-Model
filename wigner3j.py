d0f155c04f47ef60b999b675dece368c95c58bcc
Kristen Surrao <35614246+kmsurrao@users.noreply.github.com>
2023-10-19 10:50:11 -0400

    standardize docstrings

--- wigner3j.py
+++ wigner3j.py
@@ -4,10 +4,13 @@ import multiprocessing as mp
 
 def compute_3j_symbol_one_ell(l1, ell_sum_max):
     '''
-    PARAMETERS
+    ARGUMENTS
+    ---------
     l1: int, value of first ell for Wigner 3j symbol
     ell_sum_max: int, maximum l2,l3 for which to calculate Wigner 3j symbols
+
     RETURNS
+    -------
     wig3j_one_ell: 2D numpy array, indexed as wig3j_one_ell[l2,l3], contains all 3j symbols for l1
     '''
     wig.wig_table_init(ell_sum_max*2,9)
@@ -21,10 +24,14 @@ def compute_3j_symbol_one_ell(l1, ell_sum_max):
 
 def compute_3j(ell_sum_max):
     '''
-    Computes Wigner 3j symbols with multiprocessing
-    PARAMETERS
+    Computes Wigner 3j symbols with multiprocessing (parallelization)
+
+    ARGUMENTS
+    ---------
     ell_sum_max: int, maximum ell for which to calculate Wigner 3j symbols
+
     RETURNS
+    -------
     wig3j: 3D numpy array, contains wigner 3j symbols, indexed as wig3j[l1,l2,l3]
     '''
     pool = mp.Pool(16)
@@ -34,10 +41,14 @@ def compute_3j(ell_sum_max):
 
 def compute_3j_no_multiprocessing(ell_sum_max):
     '''
-    Computes Wigner 3j symbols without the multiprocessing module
-    PARAMETERS 
+    Computes Wigner 3j symbols without parallelization
+
+    ARGUMENTS 
+    ---------
     ell_sum_max: int, maximum l2,l3 for which to calculate Wigner 3j symbols
+
     RETURNS
+    -------
     wig3j: 3D numpy array, contains wigner 3j symbols, indexed as wig3j[l1,l2,l3]
     '''
     wig.wig_table_init(ell_sum_max*2,9)
